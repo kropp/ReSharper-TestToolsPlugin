f17345da76678e10ccf75d3a6e3ac9bcdbee700b
Victor Kropp <victor.kropp@gmail.com>
2013-05-11 21:19:42 +0200

    New action: toggle test ignore from Unit Test Session

---
+++ TestToolsPlugin/ToggleIgnoreTestAction.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using JetBrains.ActionManagement;
+using JetBrains.Application.DataContext;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.UnitTestExplorer;
+using JetBrains.ReSharper.UnitTestFramework;
+using JetBrains.Util;
+using DataConstants = JetBrains.ProjectModel.DataContext.DataConstants;
+
+namespace CreateTestPlugin
+{
+  [ActionHandler("TestToolsPlugin.ToggleIgnoreTest")]
+  public class ToggleIgnoreTestAction : IActionHandler
+  {
+    public bool Update(IDataContext context, ActionPresentation presentation, DelegateUpdate nextUpdate)
+    {
+      // action is in context menu, so it is updated only once when menu is shown
+      var solution = context.GetData(DataConstants.SOLUTION);
+      var elements = context.GetData(UnitTestDataConstants.UNIT_TEST_ELEMENTS);
+      if (solution == null || elements == null || elements.ExplicitElements.IsEmpty())
+      {
+        presentation.Visible = false;
+        return false;
+      }
+
+      var element = elements.ExplicitElements.First();
+      var result = solution.GetComponent<IUnitTestResultManager>().GetResult(element);
+
+      // show correct action depending on test status
+      switch (result.Status)
+      {
+        case UnitTestStatus.Success:
+        case UnitTestStatus.Ignored:
+          if (element.Explicit)
+          {
+            presentation.Text = "Unignore test";
+            presentation.Visible = true;
+          }
+          else
+            presentation.Visible = false;
+          break;
+        case UnitTestStatus.Failed:
+        case UnitTestStatus.Aborted:
+          if (!element.Explicit)
+          {
+            presentation.Text = "Ignore test";
+            presentation.Visible = true;
+          }
+          else
+            presentation.Visible = false;
+          break;
+        default:
+          presentation.Visible = false;
+          return false;
+      }
+
+      return true;
+    }
+
+    public void Execute(IDataContext context, DelegateExecute nextExecute)
+    {
+      var solution = context.GetData(DataConstants.SOLUTION);
+      if (solution == null)
+        return;
+
+      var elements = context.GetData(UnitTestDataConstants.UNIT_TEST_ELEMENTS);
+      if (elements == null || elements.ExplicitElements.IsEmpty())
+        return;
+
+      solution.GetComponent<IPsiServices>().Transactions.Execute("Toggle Ignore Test", () =>
+      {
+        foreach (var element in elements.ExplicitElements)
+          element.ToggleIgnore(solution);
+      });
+    }
+  }
+}
--- TestToolsPlugin/UnitTestElementEx.cs
+++ TestToolsPlugin/UnitTestElementEx.cs
@@ -1,9 +1,12 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.CSharp;
 using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Psi.Resolve;
 using JetBrains.ReSharper.Psi.Tree;
+using JetBrains.ReSharper.TaskRunnerFramework;
 using JetBrains.ReSharper.UnitTestFramework;
 using JetBrains.Util;
 
@@ -28,8 +31,76 @@ namespace CreateTestPlugin
       var nunitCategoryType = TypeFactory.CreateTypeByCLRName("NUnit.Framework.CategoryAttribute", psiModule, resolveContext).GetTypeElement();
       var attribute = CSharpElementFactory.GetInstance(declaration).CreateAttribute(nunitCategoryType, new[] {new AttributeValue(new ConstantValue(category, psiModule, resolveContext))}, new Pair<string, AttributeValue>[0]);
 
-      // and add it to existing declaration with
+      // and add it to existing declaration
       declaration.AddAttributeBefore(attribute, null);
+    }
+
+    public static void ToggleIgnore(this IUnitTestElement element, ISolution solution)
+    {
+      var resultManager = solution.GetComponent<IUnitTestResultManager>();
+      var result = resultManager.GetResult(element);
+      if (element.Explicit)
+      {
+        if (result.Status == UnitTestStatus.Ignored || result.Status == UnitTestStatus.Success)
+          element.UnIgnore();
+      }
+      else
+      {
+        if (result.Status == UnitTestStatus.Failed || result.Status == UnitTestStatus.Aborted)
+        {
+          element.Ignore();
+          resultManager.TestFinishing(element, string.Empty, TaskResult.Skipped);
+        }
+      }
+    }
+
+    public static void Ignore(this IUnitTestElement element)
+    {
+      var attributesOwner = element.GetDeclaredElement() as IAttributesOwner;
+      if (attributesOwner == null)
+        return;
+
+      var declaration = attributesOwner.GetDeclarations().FirstOrDefault() as IAttributesOwnerDeclaration;
+      if (declaration == null)
+        return;
+
+      var resolveContext = declaration.GetProject().GetResolveContext();
+      var psiModule = attributesOwner.Module;
+
+      // create attribute instance
+      var nunitIgnoreType = TypeFactory.CreateTypeByCLRName("NUnit.Framework.IgnoreAttribute", psiModule, resolveContext).GetTypeElement();
+      var attribute = CSharpElementFactory.GetInstance(declaration).CreateAttribute(nunitIgnoreType);
+
+      // and add it to existing declaration
+      declaration.AddAttributeBefore(attribute, null);
+    }
+
+    public static void UnIgnore(this IUnitTestElement element)
+    {
+      var attributesOwner = element.GetDeclaredElement() as IAttributesOwner;
+      if (attributesOwner == null)
+        return;
+
+      var psiModule = attributesOwner.Module;
+      var resolveContext = attributesOwner.ResolveContext;
+
+      var nunitIgnoreType = TypeFactory.CreateTypeByCLRName("NUnit.Framework.IgnoreAttribute", psiModule, resolveContext).GetTypeElement();
+      var nunitExplicitType = TypeFactory.CreateTypeByCLRName("NUnit.Framework.ExplicitAttribute", psiModule, resolveContext).GetTypeElement();
+
+      // need to check all declarations and remove all ignore/explicit attributes from all parts
+      foreach (var declaration in attributesOwner.GetDeclarations())
+      {
+        var attributesOwnerDeclaration = declaration as IAttributesOwnerDeclaration;
+        if (attributesOwnerDeclaration != null)
+          foreach (var a in attributesOwnerDeclaration.Attributes)
+          {
+            if (a.TypeReference == null)
+              continue;
+            var type = a.TypeReference.Resolve().DeclaredElement as ITypeElement;
+            if (Equals(type, nunitIgnoreType) || Equals(type, nunitExplicitType))
+              attributesOwnerDeclaration.RemoveAttribute(a);
+          }
+      }
     }
   }
 }
