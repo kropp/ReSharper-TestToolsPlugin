6a59b0a4a915178aaadb3a34c4312c32eb5e42ef
Victor Kropp <victor.kropp@gmail.com>
2013-05-17 21:48:51 +0200

    Method runner plugin

---
+++ MethodRunner/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TestToolsPlugin")]
+[assembly: AssemblyDescription("Runs single method as test")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Victor Kropp, JetBrains")]
+[assembly: AssemblyProduct("TestToolsPlugin")]
+[assembly: AssemblyCopyright("Copyright © Victor Kropp, JetBrains, 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
