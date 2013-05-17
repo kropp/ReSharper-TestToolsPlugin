a14f466a0456a97800cac3db86760defdf70b15c
Victor Kropp <victor.kropp@gmail.com>
2013-05-17 22:07:03 +0200

    Method Runner provider

---
+++ TestToolsPlugin/MethodRunnerProvider.cs
@@ -0,0 +1,43 @@
+﻿using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.UnitTestFramework;
+
+namespace CreateTestPlugin
+{
+  [UnitTestProvider]
+  public class MethodRunnerProvider : IUnitTestProvider
+  {
+    public void ExploreExternal(UnitTestElementConsumer consumer)
+    {
+    }
+
+    public void ExploreSolution(ISolution solution, UnitTestElementConsumer consumer)
+    {
+    }
+
+    public bool IsElementOfKind(IDeclaredElement declaredElement, UnitTestElementKind elementKind)
+    {
+      return false;
+    }
+
+    public bool IsElementOfKind(IUnitTestElement element, UnitTestElementKind elementKind)
+    {
+      return false;
+    }
+
+    public bool IsSupported(IHostProvider hostProvider)
+    {
+      return hostProvider is ProcessHostProvider;
+    }
+
+    public int CompareUnitTestElements(IUnitTestElement x, IUnitTestElement y)
+    {
+      return 0;
+    }
+
+    public string ID { get { return Id; } }
+    public const string Id = "MethodRunner";
+
+    public string Name { get { return Id; } }
+  }
+}
